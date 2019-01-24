c4a33bbfafa7d3f680e3ad8de1cb43e23aeb5d59
Jose Gutierrez <jagdap@gmail.com>
2019-01-24 10:42:58 -0600

    Addresses issue with converting MatOfPoint to MatOfPoint2f

--- src/main/java/team364_rpi/DynamicVisionPipeline.java
+++ src/main/java/team364_rpi/DynamicVisionPipeline.java
@@ -221,7 +221,8 @@ public class DynamicVisionPipeline implements VisionPipeline {
 
     RotatedRect rotR = new RotatedRect();
     for ( int i = 0; i < inputContours.size(); i++ ){
-      MatOfPoint2f curContour2f = new MatOfPoint2f(inputContours.get(i));
+      // THIS LINE COULD BE AN ISSUE \/\/\/\/\/
+      MatOfPoint2f curContour2f = new MatOfPoint2f(inputContours.get(i).toArray());
       rotR = Imgproc.minAreaRect(curContour2f);
       outputRects.add(rotR);
 
